a550151000915eceeab8fa4477ea00c57ad93fcc
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-03 10:44:56 -0300

    adding folder structure with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js}", 
+    "./src/**/*",
+    "./index.html",
+    "./app.js",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
