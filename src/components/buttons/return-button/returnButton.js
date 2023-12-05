07fa34699fb5945c6012d433fc03d5e18398c9e5
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-05 09:28:47 -0300

    add return button with function

--- src/components/buttons/return-button/returnButton.js
+++ src/components/buttons/return-button/returnButton.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const returnButton = document.getElementById("returnButton");
     const sound = new Audio("../../../assets/sounds/click-button.mp3");
