f57d9d68d71e3ee4e4551c9fc1b405ff24276e0c
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-05 08:49:55 -0300

    resolving issue

--- src/components/buttons/return-button/returnButton.js
+++ src/components/buttons/return-button/returnButton.js
@@ -1,11 +1,9 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const returnButton = document.getElementById("returnButton");
     const sound = new Audio("../../../assets/sounds/click-button.mp3");
  
     returnButton.addEventListener('click', (e) => {
         sound.play();
-        
+        window.history.back()
     });
  });
