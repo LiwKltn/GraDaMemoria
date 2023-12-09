f18c45526c08da1c83102d2ebe3c4d36b235fa9b
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-09 17:07:11 -0300

    fixing pair of colors and repetition

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,5 +1,8 @@
 let contentCard = document.getElementById("content-cards");
-let generatedIds = new Set();
+let generatedIds = new Map();
+let clickedContainers = document.querySelectorAll(".container-card"); // Mova a declaração aqui
+// clickedContainers.forEach(card => card.addEventListener('click', makingPairs));
+
 
 async function getData() {
   const urlHtml = "../../../public/json/card-content.json";
@@ -10,20 +13,19 @@ async function getData() {
     console.log("Error accessing JSON");
     return;
   }
-
-  let cardAmount = data.elements.length;
-  console.log("Number of elements: " + cardAmount);
   return data;
 }
+
 
 function printElement(data, i) {
   let elementArray = document.createElement("h2");
   elementArray.setAttribute(
     "class",
-    "element px-2 font-Minnie text-lg text-center"
+    "element w-full h-full font-Minnie text-xs md:text-base flex flex-wrap justify-center items-center text-center"
   );
+ 
   elementArray.textContent = data.elements[i].element;
-  
+
   return elementArray;
   
 }
@@ -32,10 +34,11 @@ function printExplanation(data, i) {
   let explanationArray = document.createElement("h2");
   explanationArray.setAttribute(
     "class",
-    "element font-mono text-base italic text-center font-bold px-2"
+    "element flex flex-wrap p-1 font-mono text-xs md:text-base italic text-center font-bold"
   );
   explanationArray.textContent = data.elements[i].explanation;
   return explanationArray;
+  
 }
 
 function printCard(data, id) {
@@ -47,40 +50,39 @@ function printCard(data, id) {
   cardImage.src =
     "https://img.freepik.com/free-photo/3d-rendering-optical-illusion_23-2150854149.jpg?w=740&t=st=1702051950~exp=1702052550~hmac=3efef74efb1008d874039b2e42742c44d824c70af120fd7d428e7bd6a06a36b7";
 
-  containerNewCard.setAttribute("class", "containerCard relative h-48 w-36 m-2");
+  containerNewCard.setAttribute("class",  "container-card relative m-2 h-28 md:h-28 w-16 md:w-24");
 
   containerNewCardPair.setAttribute(
     "class",
-    "containerCard relative h-48 w-36 m-2"
+    "container-card relative m-2 h-28 md:h-28 w-16 md:w-24"
   );
 
   newCard.setAttribute(
     "class",
-    "flip-card-back flex absolute inset-0 rounded-xl justify-center items-center shadow-xl cursor-pointer w-full h-full " +
-    (randomColor())
-  );
+    "flip-card-back flex absolute inset-0 rounded-xl justify-center items-center shadow-xl cursor-pointer w-full h-full");
 
   newCardPair.setAttribute(
-    "class",
-    "flip-card-back flex absolute inset-0 rounded-xl justify-center items-center shadow-xl cursor-pointer w-full h-full " +
-      (randomColor())
-  );
+    "class","flip-card-back flex absolute inset-0 rounded-xl justify-center items-center shadow-xl cursor-pointer w-full h-full");
 
-  cardImage.setAttribute(
-    "class",
-    "flip-card-front rounded-xl absolute inset-0 object-cover shadow-xl cursor-pointer w-full h-full"
-  );
+  cardImage.setAttribute("class", "flip-card-front rounded-xl absolute inset-0 object-cover shadow-xl cursor-pointer w-full h-full");
 
   newCard.classList.add("card");
   newCardPair.classList.add("pair");
 
   let cardImageCopy = cardImage.cloneNode(true);
 
-  let cardId = `card-${id}`;
+  let cardId = id;
   newCard.setAttribute("id", cardId);
 
-  let pairId = `pair-${cardId}`;
+  let pairId = id;
   newCardPair.setAttribute("id", pairId);
+  
+
+  if (newCard.id === newCardPair.id) {
+    let colorClass = randomColor();
+    newCard.classList.add(colorClass);
+    newCardPair.classList.add(colorClass);
+  } 
 
   contentCard.appendChild(containerNewCard);
   contentCard.appendChild(containerNewCardPair);
@@ -94,6 +96,8 @@ function printCard(data, id) {
 
   newCard.appendChild(elementTag);
   newCardPair.appendChild(elementExplanation);
+
+
 }
 
 function shuffleCards() {
@@ -101,7 +105,7 @@ function shuffleCards() {
   let container = document.getElementById("content-cards");
 
   // Get all cards and their pairs
-  let cards = document.querySelectorAll(".containerCard");
+  let cards = document.querySelectorAll(".container-card");
 
   // Shuffle the cards in place using the Fisher–Yates shuffle algorithm
   for (let i = cards.length - 1; i > 0; i--) {
@@ -133,50 +137,162 @@ function randomColor(){
 }
 
 
-async function generateCards(arg, param) {
-  let halfOfCards = param / 2;
+async function generateCards(theme, cards) {
+  let halfOfCards = cards / 2;
   let colorCard = randomColor();
-  console.log(colorCard)
+  console.log(colorCard);
+
+  const data = await getData();
+
   for (let i = 0; i < halfOfCards; i++) {
     let newId;
     do {
-      newId = getRandomId(arg);
+      newId = getRandomId(theme);
     } while (generatedIds.has(newId));
 
-    const data = await getData();
-    generatedIds.add(newId);
+    const index = generatedIds.size; // Obter o próximo índice no array
 
+    generatedIds.set(newId, index);
     printCard(data, newId);
   }
 }
 
-function getRandomId(arg) {
-  if (arg === "html") return randomHtml();
-  if (arg === "css") return randomCss();
-  if (arg === "javascript") return randomJs();
+function getRandomId(theme) {
+  if (theme === "html") return randomHtml();
+  if (theme === "css") return randomCss();
+  if (theme === "javascript") return randomJs();
   return randomAllThemes();
 }
 
 async function init() {
-  await generateCards("html", 30);
+  await generateCards("htfgml", 2);
   shuffleCards();
 }
-
-// Chame a função de inicialização
 init();
 
-// Adicionando um evento de clique nos containers existentes
-let containers = document.getElementById("content-cards");
 
-containers.addEventListener("click", (event) => {
-  let clickedContainer = event.target.closest(".containerCard");
-  if (clickedContainer) {
-    clickedContainer.classList.add("flip-card");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function makingPairs() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip-card");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
   }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.id === secondCard.id;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', makingPairs);
+  secondCard.removeEventListener('click', makingPairs);
+}
+
+function unflipCards() {
+  lockBoard = true;
   setTimeout(() => {
-    clickedContainer.classList.remove("flip-card");
-  }, 3000); // Ajuste o tempo conforme necessário
+    firstCard.classList.remove("flip-card");
+    secondCard.classList.remove("flip-card");
+    lockBoard = false;
+  }, 1500); // Ajuste o tempo conforme necessário
+}
 
-});
+clickedContainers.forEach(card => card.addEventListener('click', makingPairs));
+
+    
+   
+    
+//     const clicked = clickedContainer.querySelector("article");
+//     console.log(clicked)
+//     if (clickedContainer) {
+//       clickedContainer.classList.add("flip-card");
+    
+//       if (cardId === null) {
+//         cardId = clickedId;
+//       } else {
+//         if (clickedId === cardId) {
+
+//           hits++;
+//           // clickedContainer.setAttribute("class", "");
+
+//         } else {
+//           console.log("Mistake! Card ID:", clickedId);
+//           mistakes++;
+//           setTimeout(() => {
+//             clickedContainer.classList.remove("flip-card");
+//           }, 1000); // Ajuste o tempo conforme necessário
+//         }
+
+//         // Resetar cardId para null para a próxima jogada
+//         cardId = null;
+//       }
+//     }
+//   });
+// }
+
+// makingPairs();
 
 
+// let clickedContainer = document.querySelectorAll(".container-card");
+
+
+//  const clickedId = clickedContainer.querySelector("article").id;
+
+// const cards = document.querySelectorAll('.container-card');
+
+//   let hasFlippedCard = false;
+//   let firstCard, secondCard;
+
+//   function flipCard() {
+//     this.classList.add('flip-card');
+
+//     if (!hasFlippedCard) {
+//       hasFlippedCard = true;
+//       firstCard = this;
+//      return;
+//    }
+
+//    secondCard = this;
+//    hasFlippedCard = false;
+
+//    checkForMatch();
+//  }
+
+//  function checkForMatch() {
+//    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+//      disableCards();
+//      return;
+//    }
+
+//    unflipCards();
+//  }
+
+//  function disableCards() {
+//    firstCard.removeEventListener('click', flipCard);
+//    secondCard.removeEventListener('click', flipCard);
+//  }
+
+//  function unflipCards() {
+//    setTimeout(() => {
+//      firstCard.classList.remove('flip-card');
+//      secondCard.classList.remove('flip-card');
+//    }, 1500);
+//  }
+
+//   cards.forEach(card => card.addEventListener('click', flipCard));
