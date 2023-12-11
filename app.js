7b4b0004c8010f90362d374bb0799c6ce11de26e
Oliwia <oliwiakoltun@gmail.com>
2023-12-11 06:45:06 +0100

    created back cards

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-function iniciarJuego() {
-    var nombreJugador = document.getElementById('nombreInput').value;
-    alert('¡Juego iniciado por ' + nombreJugador + '!');
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll('.card');
   
     cards.forEach((card, index) => {
@@ -11,20 +6,33 @@ function iniciarJuego() {
         const randomX = Math.random() * (window.innerWidth - card.clientWidth);
         const randomY = Math.random() * (window.innerHeight - card.clientHeight);
   
+        // Ajusta la separación multiplicando las coordenadas aleatorias por un factor
+        const separationFactor = 1.5; // Puedes ajustar este valor según tus necesidades
+  
+        // Ajusta las coordenadas para que las cards se mantengan dentro de los límites visibles
+        const maxX = window.innerWidth - card.clientWidth * separationFactor;
+        const maxY = window.innerHeight - card.clientHeight * separationFactor;
+  
+        const adjustedX = Math.min(randomX, maxX);
+        const adjustedY = Math.min(randomY, maxY);
+  
         card.style.position = 'absolute';
-        card.style.left = `${randomX}px`;
-        card.style.top = `${randomY}px`;
+        card.style.left = `${adjustedX}px`;
+        card.style.top = `${adjustedY}px`;
       }
     });
   });
-  document.getElementById("nombreInput").addEventListener("keyup", function(event) {
+  
+  document.getElementById("nombreInput").addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
       iniciarJuego();
     }
   });
-
+  
   function iniciarJuego() {
     var nombreJugador = document.getElementById('nombreInput').value;
     alert('¡Juego iniciado por ' + nombreJugador + '!');
   }
+  
+ 
