314703ce4b33223e86c9dc06ff5bcf700b6efa00
Oliwia <oliwiakoltun@gmail.com>
2023-12-11 01:35:34 +0100

    Committing changes to dist/output.css

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 function iniciarJuego() {
     var nombreJugador = document.getElementById('nombreInput').value;
     alert('¡Juego iniciado por ' + nombreJugador + '!');
-}
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.card');
+  
+    cards.forEach((card, index) => {
+      if (index !== 0) {
+        const randomX = Math.random() * (window.innerWidth - card.clientWidth);
+        const randomY = Math.random() * (window.innerHeight - card.clientHeight);
+  
+        card.style.position = 'absolute';
+        card.style.left = `${randomX}px`;
+        card.style.top = `${randomY}px`;
+      }
+    });
+  });
+  
